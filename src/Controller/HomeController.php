46c61a5dee3449bf16257c08e959c4f789c4376a
Marco Valeri <marcovaleri@hotmail.it>
2021-09-21 10:25:05 +0100

    Add dishes menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\DishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(): Response
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(DishRepository $ds)
     {
+
+        $dishes = $ds->findAll();
+
+        $random = array_rand($dishes, 2);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            "dishes1" => $dishes[$random[0]],
+            "dishes2" => $dishes[$random[1]],
         ]);
     }
 
